1c53f01b78fb85f6a80ad7ea02d637798ea1869d
Mr-Zombii <thezombiigames@gmail.com>
2024-01-17 20:25:40 -0600

    support server mappings

--- main.py
+++ main.py
@@ -1,26 +1,28 @@
 import datetime
 import requests, json, os, time
 
-os.chdir(os.path.basename(os.path.abspath(__file__)))
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 version_manifest = "https://piston-meta.mojang.com/mc/game/version_manifest_v2.json"
 
-manifest = json.loads(requests.get(version_manifest).text)
-# baseTime = datetime.datetime(int("2019"), int("07"), int("19"))
+manifest = requests.get(version_manifest).json()
 if not os.path.exists("release"): os.mkdir("release")
 if not os.path.exists("snapshot"): os.mkdir("snapshot")
 for x in manifest["versions"]:
-    # print(x["id"], x["type"])
-    t = x["releaseTime"].split(":")[0].split("T")[0].split("-")
-    releaseTime = datetime.datetime(int(t[0]), int(t[1]), int(t[2]))
-    if not os.path.exists(x["type"]+"/"+x["id"]+".txt"):
-        vManifest = json.loads(requests.get(x["url"]).text)
+    if not os.path.exists(x["type"]+"/"+x["id"]+"/client.txt"):
+        vManifest = requests.get(x["url"]).json()
         try:
-            print()
-            print(x["type"], x["id"], t[0]+"-"+t[1]+"-"+t[2], "->", vManifest["downloads"]["client_mappings"]["url"])
-            f = open(x["type"]+"/"+x["id"]+".txt", "w")
-            f.write(requests.get(vManifest["downloads"]["client_mappings"]["url"]).text)
-            f.close()
+            os.makedirs(x["type"]+"/"+x["id"])
+            print(x["type"], x["id"], "->", vManifest["downloads"]["client_mappings"]["url"])
+            if "client_mappings" in vManifest["downloads"]:
+                f = open(x["type"]+"/"+x["id"]+"/client.txt", "wb")
+                f.write(requests.get(vManifest["downloads"]["client_mappings"]["url"]).content)
+                f.close()
+            print(x["type"], x["id"], "->", vManifest["downloads"]["server_mappings"]["url"])
+            if "server_mappings" in vManifest["downloads"]:
+                f = open(x["type"]+"/"+x["id"]+"/server.txt", "wb")
+                f.write(requests.get(vManifest["downloads"]["server_mappings"]["url"]).content)
+                f.close()
         except: 1
 
 for root, dirs, files in os.walk("."):
